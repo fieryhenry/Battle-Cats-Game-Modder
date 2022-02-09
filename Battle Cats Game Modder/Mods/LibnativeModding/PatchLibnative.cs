95b8654941f9ef71d33b20e3c06cbcb5b0bfc4fb
fieryhenry <git@henrythomas.me.uk>
2022-02-09 19:58:24 +0000

    Add project files.

---
+++ Battle Cats Game Modder/Mods/LibnativeModding/PatchLibnative.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Battle_Cats_Game_Modder.Program;
+
+
+namespace Battle_Cats_Game_Modder.Mods.LibnativeModding
+{
+    public class PatchLibnative
+    {
+        static readonly List<Tuple<string, byte[], int>> architectures = new()
+        {
+            Tuple.Create("x86", new byte[] { 0x55, 0x89, 0xe5, 0x83, 0xe4, 0xfc, 0xb8, 5, 0, 0, 0, 0x89, 0xec, 0x5d, 0xc3 }, 7),
+            Tuple.Create("x86_64", new byte[] { 0xB8, 5, 0, 0, 0, 0xC3, 0x66, 0x2E, 0x0F, 0x1F, 0x84 }, 1),
+            Tuple.Create("armeabi-v7a", new byte[] { 0x05, 0, 0xA0, 0xE3, 0x1E, 0xFF, 0x2F, 0xE1 }, 0),
+            Tuple.Create("arm64-v8a", new byte[] { 0xA0, 0x00, 0x80, 0x52, 0xC0, 0x03, 0x5F, 0xD6 }, 0),
+        };
+        public static int Search_pos(string path)
+        {
+            List<string> architecture_str = new();
+            foreach (Tuple<string, byte[], int> architecture in architectures)
+            {
+                architecture_str.Add(architecture.Item1);
+            }
+            ColouredText($"&What architecture are you using?:\n&{CreateOptionsList<string>(architecture_str.ToArray())}");
+            int id = Inputed() - 1;
+            Tuple<string, byte[], int> curr_architecture = architectures[id];
+            int pos = Search(path, curr_architecture.Item2)[0] + curr_architecture.Item3;
+            if (pos < 5000)
+            {
+
+                Error("Error. You must have already done this fix, or this is a bug. If it is a bug, please report this on discord in #bug-reports");
+            }
+            return pos;
+        }
+        public static void MD5Lib()
+        {
+            string path;
+            Console.WriteLine("Please select an so file");
+            OpenFileDialog fd = new()
+            {
+                Filter = "files (*.so)|*.so"
+            };
+            if (fd.ShowDialog() != DialogResult.OK)
+            {
+                Console.WriteLine("Please select .so files");
+                Main();
+            }
+            path = fd.FileName;
+
+            int pos = Search_pos(path);
+            using var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
+            stream.Position = pos;
+            stream.WriteByte(0);
+            Console.WriteLine("Successfully patched game code to not check the md5 hashes of .pack and .list files.\nYou shouldn't need to re-hack this after you edit the .pack and .lists again, the patch should remain until the game updates.");
+        }
+    }
+}
---
+++ Battle Cats Game Modder/Program.cs
@@ -0,0 +1,262 @@
+﻿using Battle_Cats_Game_Modder.Mods.PackFileModding;
+using Battle_Cats_Game_Modder.Mods.LibnativeModding;
+using Battle_Cats_Game_Modder.Mods.GameDataModding;
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+
+namespace Battle_Cats_Game_Modder;
+public static class Program
+{
+    public static string multipleVals = "(You can enter multiple numbers seperated by spaces to edit multiple at once)";
+    public static void Main()
+    {
+        string[] options =
+        {
+            "Decrypt .pack files",
+            "Encrypt folder to .pack and .list files",
+            "Patch libnative-lib.so to remove md5 sum check",
+            "Modify game data"
+        };
+        ColouredText($"&What do you want to do?\n&{CreateOptionsList<string>(options)}");
+        int answer = Inputed();
+        switch (answer)
+        {
+            case 1: DecryptPack.Decrypt(); break;
+            case 2: EncryptPack.EncryptData(); break;
+            case 3: PatchLibnative.MD5Lib(); break;
+            case 4: ModifyGameData(); break;
+            default: Console.WriteLine("Please enter a recognised number"); break;
+        }
+        Console.WriteLine("Press enter to continue...");
+        Console.ReadLine();
+        Main();
+    }
+    public static int[] ConvertCharArrayToIntArray(char[] input)
+    {
+        return Array.ConvertAll(input, (char x) => int.Parse(string.Format("{0}", x)));
+    }
+    public static void Error(string text = "Error, a position couldn't be found, please report this in #bug-reports on discord")
+    {
+        Console.WriteLine(text + "\nPress enter to continue");
+        Console.ReadLine();
+        Main();
+    }
+    public static double ConvertBytesToMegabytes(long bytes)
+    {
+        return Math.Round(bytes / 1024f / 1024f, 2);
+    }
+    public static void ClearCurrentConsoleLine()
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, currentLineCursor);
+    }
+    public static byte[] StringToByteArray(string hex)
+    {
+        return Enumerable.Range(0, hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(hex.Substring(x, 2), 16)).ToArray();
+    }
+    public static int Inputed()
+    {
+        int input = 0;
+        try
+        {
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (OverflowException)
+        {
+            ColouredText("Input number was too large\n", ConsoleColor.White, ConsoleColor.DarkRed);
+            Main();
+        }
+        catch (FormatException)
+        {
+            ColouredText("Input given was not a number or it wasn't an integer\n", ConsoleColor.White, ConsoleColor.DarkRed);
+            Main();
+        }
+        return input;
+    }
+    public static void ModifyGameData()
+    {
+        string[] options =
+        {
+            "Modify unit.csv (cat data)",
+            "Modify stage.csv (stage data)",
+            "Modify t_unit.csv (enemy data)"
+        };
+        ColouredText($"&What do you want to do?\n&{CreateOptionsList<string>(options)}");
+        int answer = Inputed();
+        switch (answer)
+        {
+            case 1: UnitMod.Unitcsv(); break;
+            case 2: StageMod.Stagecsv(); break;
+            case 3: EnemyMod.EnemyCSV(); break;
+            default: Console.WriteLine("Please enter a recognised number"); break;
+        }
+    }
+    public static string CreateOptionsList<T>(string[] options, T[]? extravalues = null, bool numerical = true, bool skipZero = false, string? first = null, bool color = true)
+    {
+        string toOutput = "";
+        for (int i = 0; i < options.Length; i++)
+        {
+            if (extravalues != null && skipZero)
+            {
+                int val = Convert.ToInt32(extravalues[i]);
+                if (val == 0)
+                {
+                    continue;
+                }
+            }
+            if (numerical)
+            {
+                if (first != null)
+                {
+                    if (i == 0)
+                    {
+                        toOutput += $"{i}.& ";
+                        toOutput += first;
+                        options[i] = "&";
+                    }
+                    else
+                    {
+                        toOutput += $"&{i}.& ";
+                    }
+                }
+                else
+                {
+                    toOutput += $"&{i + 1}.& ";
+                }
+            }
+            toOutput += options[i];
+            if (extravalues != null)
+            {
+                try
+                {
+                    toOutput += $" &: {extravalues[i]}&";
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            toOutput += "\n";
+
+        }
+        if (color)
+        {
+            return toOutput;
+        }
+        return toOutput.Replace("&", "");
+    }
+    public static int[] Search(string path, byte[] conditions, bool negative = false, int startpoint = 0, byte[] mult = null, int endpoint = -1, int stop_after = -1)
+    {
+        byte[] allData = File.ReadAllBytes(path);
+        if (mult == null)
+        {
+            mult = new byte[conditions.Length];
+        }
+        if (endpoint == -1)
+        {
+            endpoint = allData.Length - conditions.Length;
+        }
+
+        int pos = 0;
+        int num = 1;
+        List<int> values = new();
+        int stop_count = 0;
+        if (negative)
+        {
+            num = -1;
+        }
+        for (int i = startpoint; i < endpoint; i += num)
+        {
+            if (stop_after > -1 && stop_count >= stop_after && values.Count > 0)
+            {
+                break;
+            }
+            int count = 0;
+            for (int j = 0; j < conditions.Length; j++)
+            {
+                if (negative)
+                {
+                    try
+                    {
+                        if (allData[i - j] == conditions[conditions.Length - 1 - j] || mult[conditions.Length - 1 - j] == 1)
+                        {
+                            if (stop_count > 0)
+                            {
+                                stop_count = 0;
+                            }
+                            count++;
+                            pos = i;
+                        }
+                        else
+                        {
+                            if (count > 0)
+                            {
+                                stop_count++;
+                            }
+                            count = 0;
+                        }
+                    }
+                    catch
+                    {
+                        if (values[0] > 0)
+                        {
+                            i = allData.Length;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (allData[i + j] == conditions[j] || mult[j] == 1)
+                    {
+                        count++;
+                        pos = i;
+                    }
+                    else
+                    {
+                        if (count > 0)
+                        {
+                            stop_count++;
+                        }
+                        count = 0;
+                    }
+                }
+            }
+            if (count >= conditions.Length)
+            {
+                values.Add(pos);
+                stop_count = 0;
+            }
+        }
+        if (values.Count == 0)
+        {
+            values.Add(0);
+        }
+        return values.ToArray();
+    }
+    public static void ColouredText(string input, ConsoleColor Base = ConsoleColor.White, ConsoleColor New = ConsoleColor.DarkYellow)
+    {
+        string[] split = input.Split('&');
+        try
+        {
+            Console.ForegroundColor = New;
+            for (int i = 0; i < split.Length; i += 2)
+            {
+                Console.ForegroundColor = New;
+                Console.Write(split[i]);
+                Console.ForegroundColor = Base;
+                if (i == split.Length - 1)
+                {
+                    break;
+                }
+                Console.Write(split[i + 1]);
+            }
+            Console.ForegroundColor = Base;
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+    }
+}
